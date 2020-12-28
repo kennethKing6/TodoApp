95ef2ae8586e344a0625171246e3750347acac0f
Kouadio Kenneth <34052222+kennethKing6@users.noreply.github.com>
2020-12-27 22:59:42 -0800

    entry point to app naviagtion and redux

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from "react-redux";
-import {store} from './src/redux' 
+import {store} from './src/redux' ;
+import AppNavigations from './src/navigations';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AppNavigations/>
     </Provider>
   );
 }
